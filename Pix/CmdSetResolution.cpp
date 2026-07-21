3dde92ec1de868ad267c0d6b959f4615054cda65
sposadam16 <santiago.posada_manrique@lcieducation.net>
2026-07-20 18:06:41 -0700

    Assigment 2
    Created and rgb X
    Created and triangle filled with rgb

--- Pix/CmdSetResolution.cpp
+++ Pix/CmdSetResolution.cpp
@@ -2,23 +2,43 @@
 
 #include <XEngine.h>
 
+#include <exception>
+
 float gResolutionX = 0.0f;
 float gResolutionY = 0.0f;
 
 bool CmdSetResolution::Execute(const std::vector<std::string>& params)
 {
-	// Need at least 2 params for width, height
-	if (params.size() < 2)
+	// Width and height are required; pixel size and grid visibility are optional.
+	if (params.size() < 2 || params.size() > 4)
 		return false;
 
-	const int width = stoi(params[0]);
-	const int height = stoi(params[1]);
+	int width;
+	int height;
+	int pixelSize = 1;
+	try
+	{
+		width = std::stoi(params[0]);
+		height = std::stoi(params[1]);
+		if (params.size() > 2)
+			pixelSize = std::stoi(params[2]);
+	}
+	catch (const std::exception&)
+	{
+		return false;
+	}
 
-	// Optional third param for pixel size
-	const int pixelSize = params.size() > 2 ? stoi(params[2]) : 1;
+	if (width <= 0 || height <= 0 || pixelSize <= 0)
+		return false;
 
-	// Optional fourth param for show grid
-	const bool showGrid = params.size() > 3 && params[3] == "true";
+	bool showGrid = false;
+	if (params.size() > 3)
+	{
+		if (params[3] == "true")
+			showGrid = true;
+		else if (params[3] != "false")
+			return false;
+	}
 
 	// Cache resolution
 	gResolutionX = (float)width;
